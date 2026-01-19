882f4243bdc1b2201c76948d9b58c4299da09a14
Bruno Henriques <zettca@users.noreply.github.com>
2026-01-19 13:50:16 +0000

    test(storybook): fix playgroud grid & bg colors
    reduce our storybook overrides so that we can use the storybook's
    background grid & colors to test components in different scenarios

--- .storybook/decorators/withThemeDecorator.decorator.tsx
+++ .storybook/decorators/withThemeDecorator.decorator.tsx
@@ -1,17 +1,9 @@
 import createCache from "@emotion/cache";
-import { Global } from "@emotion/react";
 import { DecoratorHelpers } from "@storybook/addon-themes";
 import type { Decorator } from "@storybook/react-vite";
-import { useEffect } from "storybook/preview-api";
 import { HvProvider, themes } from "@hitachivantara/uikit-react-core";
 
-import { getStoryStyles } from "../theme/styles/story";
-import {
-  getInitialTheme,
-  getThemesList,
-  setLocalTheme,
-  useDarkClass,
-} from "./utils";
+import { getThemesList, useDarkClass } from "./utils";
 
 const { initializeThemeState, pluckThemeFromContext } = DecoratorHelpers;
 
@@ -27,44 +19,31 @@ export const emotionCache = createCache({
 });
 
 export const withThemeDecorator = (): Decorator => {
-  const defaultTheme = getInitialTheme(themesList).label;
-  initializeThemeState(themesLabels, defaultTheme);
+  initializeThemeState(themesLabels, "pentaho light");
 
   return (Story, context) => {
     const selectedTheme = pluckThemeFromContext(context);
     const { themeOverride } = context.parameters.themes ?? {};
 
-    const selected = themeOverride || selectedTheme || defaultTheme;
+    const selected = themeOverride || selectedTheme;
     const [theme, mode] = selected.split(" ");
-
-    const base = (themes as any)[theme] ?? themes.next;
-    const storyStyles = getStoryStyles(base.colors[mode].bgPage);
 
     const containerRef = useDarkClass(mode);
 
-    // Listen for theme changes and keep track in local storage
-    useEffect(() => {
-      if (context.globals.theme) setLocalTheme(context.globals.theme);
-    }, [context.globals.theme]);
-
     return (
-      <>
-        <Global styles={storyStyles} />
-        <HvProvider
-          emotionCache={emotionCache}
-          cssTheme="scoped"
-          theme={themes[theme as keyof typeof themes]}
-          colorMode={mode}
+      <HvProvider
+        emotionCache={emotionCache}
+        theme={themes[theme as keyof typeof themes]}
+        colorMode={mode}
+      >
+        <div
+          ref={containerRef}
+          className="hv-story-sample"
+          style={{ padding: 20 }}
         >
-          <div
-            ref={containerRef}
-            className="hv-story-sample"
-            style={{ padding: 20 }}
-          >
-            <Story />
-          </div>
-        </HvProvider>
-      </>
+          <Story />
+        </div>
+      </HvProvider>
     );
   };
 };
