272c951b7f6f6379aedb686e9e5ddad3dd80eb51
Bruno Henriques <bruno.henriques@hitachivantara.com>
2026-01-21 12:26:00 +0000

    feat: bump lib to ES2023 (#5063)
    Co-authored-by: Bruno Henriques <zettca@users.noreply.github.com>

--- .config/vite.config.ts
+++ .config/vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
     react(),
   ],
   build: {
-    target: "ES2022",
+    target: "baseline-widely-available",
     minify: false,
     emptyOutDir: true,
     lib: {
