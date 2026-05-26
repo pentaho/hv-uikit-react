17af51fd7c65909f123b256d7865d46a2333e990
Bruno Henriques <638946+zettca@users.noreply.github.com>
2026-05-26 16:53:07 +0100

    build(icons): remove cpy-cli & tsx (#5176)
    * build(icons): replace cpy-cli with vite-plugin-static-copy
    
    * build(icons): replace tsx runner with node

--- packages/icons/vite.config.ts
+++ packages/icons/vite.config.ts
@@ -1,3 +1,12 @@
+import { mergeConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
 import viteConfig from "../../.config/vite.config";
 
-export default viteConfig;
+export default mergeConfig(viteConfig, {
+  plugins: [
+    viteStaticCopy({
+      targets: [{ src: "sprites", dest: "./" }],
+    }),
+  ],
+});
