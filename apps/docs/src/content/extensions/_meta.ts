5308fd8ff029f68ceb49dd7b3e46ad2881027d5f
Bruno Henriques <638946+zettca@users.noreply.github.com>
2026-07-08 16:00:01 +0100

    docs: move lab/pentaho/charts to extensions

---
+++ apps/docs/src/content/extensions/_meta.ts
@@ -0,0 +1,29 @@
+export default {
+  // eslint-disable-next-line sort-keys
+  "-- Pentaho": { title: "Pentaho", type: "separator" },
+  pentaho: "Get started",
+  canvas: { title: "Canvas" },
+
+  "-- Code Editor": { type: "separator", title: "Code Editor" },
+  "code-editor": "Code Editor",
+
+  // eslint-disable-next-line sort-keys
+  "-- Charts": { type: "separator", title: "Charts" },
+  charts: "Get started",
+  "base-chart": "Base Chart",
+  "bar-chart": "Bar Chart",
+  "line-chart": "Line Chart",
+  "donut-chart": "Donut Chart",
+  "scatter-plot": "Scatter Plot",
+  treemap: "Treemap",
+  heatmap: "Heatmap",
+  "confusion-matrix": "Confusion Matrix",
+
+  // eslint-disable-next-line sort-keys
+  "-- Lab": { title: "Lab", type: "separator" },
+  lab: "Get started",
+  // eslint-disable-next-line sort-keys
+  dashboard: "Dashboard",
+  "step-navigation": "Step Navigation",
+  wizard: "Wizard",
+};
