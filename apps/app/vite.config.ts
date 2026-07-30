6205b7a5aee4331a896652fffe4a119b9a0a5f1b
Bruno Henriques <638946+zettca@users.noreply.github.com>
2026-07-30 11:35:56 +0100

    chore(deps): bump app's react & react-router versions (#5220)
    * chore(deps): force-bump postcss
    * chore(deps): bump app's react & react-router versions

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   resolve: {
     conditions: ["@uikit/source"],
+    dedupe: ["react", "react-dom"], // ensure `app`'s React@19 is loaded
   },
   plugins: [unoCSS(), react()],
 });
