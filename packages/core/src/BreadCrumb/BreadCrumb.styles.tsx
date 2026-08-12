d2af495d25aa30423be01af590921aa703523964
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2026-08-12 11:22:42 +0100

    refactor(core): make HvBreadCrumb pentaho classes the default

--- packages/core/src/BreadCrumb/BreadCrumb.styles.tsx
+++ packages/core/src/BreadCrumb/BreadCrumb.styles.tsx
@@ -3,14 +3,23 @@ import { theme } from "@pentaho/uikit-styles";
 
 export const { staticClasses, useClasses } = createClasses("HvBreadCrumb", {
   root: { display: "flex", alignItems: "center", zIndex: 0 },
-  link: {},
+  link: {
+    height: 24,
+    display: "flex",
+    alignItems: "center",
+    padding: `0px ${theme.space.xxs}`,
+    fontWeight: theme.fontWeights.normal,
+  },
   orderedList: {
     display: "flex",
     paddingLeft: 0,
     marginLeft: `-${theme.space.xs}`,
   },
-  currentPage: { padding: `8px ${theme.space.xs}` },
+  currentPage: {
+    padding: `0 ${theme.space.xxs}`,
+    fontWeight: theme.fontWeights.semibold,
+  },
   centerContainer: {},
-  separatorContainer: {},
+  separatorContainer: { padding: `0 ${theme.space.xxs}` },
   a: {},
 });
