43f3a0a3df2f92fd4e7eed1c5a49f7aba58b44f9
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2026-08-12 10:23:47 +0100

    refactor(core): make HvBaseRadio pentaho style the default

--- packages/core/src/themes/pentaho.ts
+++ packages/core/src/themes/pentaho.ts
@@ -24,7 +24,6 @@ import type {
   HvDropdownPanelProps,
 } from "../BaseDropdown";
 import type { HvBaseInputProps } from "../BaseInput";
-import type { HvBaseRadioProps } from "../BaseRadio";
 import type { HvRadioIconProps } from "../BaseRadio/RadioIcon";
 import type { HvBaseSwitchProps } from "../BaseSwitch";
 import type { HvBreadCrumbProps } from "../BreadCrumb";
@@ -283,13 +282,6 @@ export const pentaho = mergeTheme(pentahoBase, {
         },
       },
     } satisfies CSSClasses<HvCheckBoxIconProps>,
-    HvBaseRadio: {
-      classes: {
-        root: {
-          borderRadius: theme.radii.base,
-        },
-      },
-    } satisfies CSSClasses<HvBaseRadioProps>,
     HvRadioIcon: {
       classes: {
         checked: {
