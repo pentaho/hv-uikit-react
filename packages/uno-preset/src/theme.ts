bcc7cde699d66de3fa458bf0f9871e6b2568e059
Bruno Henriques <638946+zettca@users.noreply.github.com>
2026-08-12 12:11:23 +0100

    fix(uno): min/max-w using theme breakpoint values (#5232)
    fix(uno): min/max-w using breakpoint values
    
    max-w- and min-w- use breakpoint values instead of spacing
    fix font-sans value

--- packages/uno-preset/src/theme.ts
+++ packages/uno-preset/src/theme.ts
@@ -1,6 +1,6 @@
 import type { ThemeExtender } from "@unocss/core";
 import type { Theme } from "@unocss/preset-wind3";
-import { next as hvTheme, theme } from "@hitachivantara/uikit-styles";
+import { pentaho as hvTheme, theme } from "@hitachivantara/uikit-styles";
 
 // #region theme conversion utils
 const { light: defaultColors } = hvTheme.colors;
@@ -25,6 +25,8 @@ export const extendTheme: ThemeExtender<Theme> = (baseTheme) => ({
     ...theme.radii,
   },
   breakpoints: Object.fromEntries(hvBreakpoints),
+  maxWidth: Object.fromEntries(hvBreakpoints),
+  minWidth: Object.fromEntries(hvBreakpoints),
   containers: Object.fromEntries(
     hvBreakpoints.map(([k, v]) => [k, `(min-width: ${v})`]),
   ),
@@ -47,10 +49,9 @@ export const extendTheme: ThemeExtender<Theme> = (baseTheme) => ({
 
   // typography
   fontFamily: {
+    ...baseTheme.fontFamily,
     body: hvTheme.fontFamily.body,
     sans: hvTheme.fontFamily.body,
-    serif: hvTheme.fontFamily.body,
-    mono: "monospace",
   },
   lineHeight: { DEFAULT: hvTheme.lineHeights.base, ...hvTheme.lineHeights },
   fontSize: { DEFAULT: hvTheme.fontSizes.base, ...hvTheme.fontSizes },
