a8c51d79d7002219ad29e4810e836f2357b84f5f
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2026-08-12 15:05:41 +0100

    refactor(core): make HvButton pentaho style the default

--- packages/core/src/Button/Button.styles.ts
+++ packages/core/src/Button/Button.styles.ts
@@ -41,6 +41,14 @@ export const { staticClasses, useClasses } = createClasses("HvButton", {
       lineHeight: 0,
       minWidth: 16,
     },
+
+    ":where(:not($disabled,$contained))": {
+      "&[data-color=positive]": { color: theme.colors.positive },
+      "&[data-color=warning]": { color: theme.colors.warning },
+      "&[data-color=negative]": { color: theme.colors.negative },
+      ":hover": { backgroundColor: theme.colors.primaryDimmed },
+      ":active": { backgroundColor: theme.colors.primarySubtle },
+    },
   },
   /** applied to the _left_ icon container */
   startIcon: {
@@ -55,11 +63,17 @@ export const { staticClasses, useClasses } = createClasses("HvButton", {
   disabled: {
     cursor: "not-allowed",
     color: theme.colors.textDisabled,
-    backgroundColor: "transparent",
+    backgroundColor: theme.colors.bgDisabled,
     borderColor: "transparent",
     ":hover, :focus-visible": {
-      backgroundColor: "transparent",
+      backgroundColor: theme.colors.bgDisabled,
       borderColor: "transparent",
+    },
+    ":not($ghost)": {
+      borderColor: "transparent",
+      "&,:hover,:active": {
+        backgroundColor: theme.colors.bgDisabled,
+      },
     },
   },
   /** applied to the root element when is icon-only */
@@ -80,14 +94,86 @@ export const { staticClasses, useClasses } = createClasses("HvButton", {
       ":active": {
         backgroundColor: theme.mix("var(--color)", 0.7, "black"),
       },
+      color: "#FFFFFF",
+      "&[data-color=primary]": {
+        backgroundColor: theme.colors.primary,
+        ":hover": { backgroundColor: theme.colors.primaryStrong },
+        ":active": { backgroundColor: theme.colors.primaryDeep },
+      },
+      "&[data-color=positive]": {
+        ":hover": { backgroundColor: theme.colors.positiveDeep },
+        ":active": { backgroundColor: theme.colors.positiveStrong },
+      },
+      "&[data-color=warning]": {
+        ":hover": { backgroundColor: theme.colors.warningDeep },
+        ":active": { backgroundColor: theme.colors.warningStrong },
+      },
+      "&[data-color=negative]": {
+        ":hover": { backgroundColor: theme.colors.negativeDeep },
+        ":active": { backgroundColor: theme.colors.negativeStrong },
+      },
     },
   },
   /** applied to the root element when using the `subtle` variant */
   subtle: {
-    borderColor: "currentcolor",
+    borderColor: theme.alpha("currentcolor", 0.4),
+    ":where(:not($disabled))": {
+      "&[data-color=primary]": {
+        borderColor: theme.colors.primarySubtle,
+        backgroundColor: theme.colors.primaryDimmed,
+      },
+      "&[data-color=secondary]": {
+        borderColor: theme.colors.border,
+        backgroundColor: theme.colors.bgPage,
+      },
+      ":hover": {
+        backgroundColor: theme.colors.primaryDimmed,
+      },
+      ":active": {
+        borderColor: "transparent",
+        backgroundColor: theme.colors.primarySubtle,
+      },
+      "&[data-color=positive]": {
+        borderColor: theme.colors.positiveBorder,
+        backgroundColor: theme.colors.positiveDimmed,
+        ":hover": { backgroundColor: theme.colors.positiveSubtle },
+        ":active": {
+          backgroundColor: theme.colors.positiveBorder,
+        },
+      },
+      "&[data-color=warning]": {
+        borderColor: theme.colors.warningBorder,
+        backgroundColor: theme.colors.warningDimmed,
+        ":hover": { backgroundColor: theme.colors.warningSubtle },
+        ":active": { backgroundColor: theme.colors.warningBorder },
+      },
+      "&[data-color=negative]": {
+        borderColor: theme.colors.negativeBorder,
+        backgroundColor: theme.colors.negativeDimmed,
+        ":hover": { backgroundColor: theme.colors.negativeSubtle },
+        ":active": {
+          backgroundColor: theme.colors.negativeBorder,
+        },
+      },
+    },
   },
   /** applied to the root element when using the `ghost` variant */
-  ghost: {},
+  ghost: {
+    ":where(:not($disabled))": {
+      "&[data-color=positive]": {
+        ":hover": { backgroundColor: theme.colors.positiveDimmed },
+        ":active": { backgroundColor: theme.colors.positiveSubtle },
+      },
+      "&[data-color=warning]": {
+        ":hover": { backgroundColor: theme.colors.warningDimmed },
+        ":active": { backgroundColor: theme.colors.warningSubtle },
+      },
+      "&[data-color=negative]": {
+        ":hover": { backgroundColor: theme.colors.negativeDimmed },
+        ":active": { backgroundColor: theme.colors.negativeSubtle },
+      },
+    },
+  },
   /** applied to the root element when using the `semantic` variant */
   semantic: {
     color: theme.colors.textDark,
