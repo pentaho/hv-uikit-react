791466f61c584557e3d10e524d3cdef93fbd6593
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2026-08-12 10:22:22 +0100

    refactor(core): make HvPagination pentaho style the default

--- packages/core/src/themes/pentaho.ts
+++ packages/core/src/themes/pentaho.ts
@@ -52,7 +52,6 @@ import type { HvInlineEditorProps } from "../InlineEditor";
 import type { HvInputProps } from "../Input";
 import type { HvMultiButtonProps } from "../MultiButton";
 import type { HvNumberInputProps } from "../NumberInput";
-import type { HvPaginationProps } from "../Pagination";
 import type { HvSectionProps } from "../Section";
 import type { HvSelectProps } from "../Select";
 import type { HvSnackbarContentProps, HvSnackbarProps } from "../Snackbar";
@@ -704,13 +703,6 @@ export const pentaho = mergeTheme(pentahoBase, {
         },
       },
     } satisfies CSSClasses<HvDropdownButtonProps>,
-    HvPagination: {
-      classes: {
-        icon: {
-          fontSize: 16,
-        },
-      },
-    } satisfies CSSClasses<HvPaginationProps>,
     HvHeader: {
       classes: {
         root: {
