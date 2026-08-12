5a2fadfa49d2aa57638aa5bb59bfca07c0248bb5
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2026-08-12 12:06:28 +0100

    refactor(core): make HvBadge pentaho style the default

--- packages/core/src/Badge/Badge.styles.tsx
+++ packages/core/src/Badge/Badge.styles.tsx
@@ -22,7 +22,8 @@ export const { staticClasses, useClasses } = createClasses("HvBadge", {
       left: "100%",
     },
     ...theme.typography.caption2,
-    color: theme.colors.textDimmed,
+    "--bg-color": theme.colors.textSubtle,
+    color: "light-dark(#FFFFFF, #000000)",
     borderRadius: theme.radii.full,
     backgroundColor: `var(--bg-color, ${theme.colors.text})`,
     lineHeight: "16px",
@@ -41,6 +42,21 @@ export const { staticClasses, useClasses } = createClasses("HvBadge", {
       width: 8,
       padding: 0,
     },
+
+    "&[data-color='textSubtle']:not(:empty)": {
+      color: theme.colors.textSubtle,
+      backgroundColor: theme.colors.bgPageSecondary,
+    },
+
+    ...Object.fromEntries(
+      (["primary", "positive", "warning", "negative"] as const).map((color) => [
+        `&[data-color='${color}']:not(:empty)`,
+        {
+          color: theme.colors[color],
+          backgroundColor: theme.colors[`${color}Dimmed`],
+        },
+      ]),
+    ),
   },
   /** applied to the badge when it's hidden */
   badgeHidden: {
