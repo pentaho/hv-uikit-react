1fa2ec2b37de91ea094da69570adfeff9906685a
Bruno Henriques <638946+zettca@users.noreply.github.com>
2026-08-20 14:26:52 +0100

    chore: remove dead story files

--- packages/core/src/Card/Card.stories.tsx
+++ packages/core/src/Card/Card.stories.tsx
@@ -4,10 +4,11 @@ import {
   HvCardContent,
   HvCardHeader,
   HvCardMedia,
+  HvTypography,
   type HvCardProps,
 } from "@pentaho/uikit-react-core";
+import { Level2Average } from "@pentaho/uikit-react-icons";
 
-import { Main as MainStory } from "./stories/Main";
 import { Variants as VariantsStory } from "./stories/Variants";
 
 const meta: Meta<typeof HvCard> = {
@@ -30,7 +31,31 @@ export const Main: StoryObj<HvCardProps> = {
     classes: { control: { disable: true } },
     icon: { control: { disable: true } },
   },
-  render: (args) => <MainStory {...args} />,
+  render: (args) => (
+    <HvCard style={{ width: 360 }} {...args}>
+      <HvCardHeader
+        title="Asset Avatar L90"
+        subheader="Compressor"
+        icon={<Level2Average color={args?.statusColor} />}
+      />
+      <HvCardMedia
+        component="img"
+        alt="Compressor"
+        height={140}
+        image="https://i.imgur.com/bxPPTD3.png"
+      />
+      <HvCardContent>
+        <div style={{ paddingTop: "20px" }}>
+          <HvTypography variant="label">ID</HvTypography>
+          <HvTypography>2101cad3-7cd4-1000-bdp95-d8c497176e7c</HvTypography>
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          <HvTypography variant="label">Last connected</HvTypography>
+          <HvTypography>Aug 30, 2017 12:27:53 PM</HvTypography>
+        </div>
+      </HvCardContent>
+    </HvCard>
+  ),
 };
 
 export const Variants: StoryObj<HvCardProps> = {
